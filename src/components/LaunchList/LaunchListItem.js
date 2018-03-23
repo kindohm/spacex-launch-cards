6a64d75f0a0f96723c9bbd7fd0d7e8b82f477555
Mike Hodnick <mike@kindohm.com>
2018-03-23 13:53:11 -0500

    better success fail styling

--- src/components/LaunchList/LaunchListItem.js
+++ src/components/LaunchList/LaunchListItem.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const LaunchListItem = ({success, flightNumber, missionPatchUrl, selectLaunch }) => (
+const LaunchListItem = ({ success, flightNumber, missionPatchUrl, selectLaunch }) => (
 
     <div className="hoverable" style={{ cursor: 'pointer' }} onClick={selectLaunch.bind(this, flightNumber)}>
-        <div className={`card horizontal ${success ? '' : 'red lighten-3'}`} style={{ height: '80px' }}>
+        <div className={'card horizontal'} style={{ height: '80px' }}>
             <div className="card-image valign-wrapper hide-on-small-only">
                 <img src={missionPatchUrl} alt={flightNumber} style={{ margin: '10px', maxWidth: '50px', maxHeight: '80px' }} />
             </div>
             <div className="card-stacked">
-                <div className="card-content valign-wrapper">
-                    <p>Flight #{flightNumber}</p>
+                <div className="card-content valign-wrapper row" style={{marginBottom:'0px'}}>
+                    <div className="col s6">Flight #{flightNumber}</div>
+                    <div className="col s6 right-align">
+                        <i className={`${success ? 'green-text' : 'red-text'} material-icons`}>
+                            {success ? 'check' : 'close'}
+                        </i>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 );
 
-LaunchListItem.propTypes= {
+LaunchListItem.propTypes = {
     flightNumber: PropTypes.number,
     missionPatchUrl: PropTypes.string,
     selectLaunch: PropTypes.func,
